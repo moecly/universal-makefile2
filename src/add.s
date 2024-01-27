7c44a0c43a5161f5307d5b29c3e52ae6447c1ac7
moecly <1349269087ABC@gmail.com>
2024-01-27 15:01:51 +0800

    添加.s文件的编译支持

---
+++ src/add.s
@@ -0,0 +1,6 @@
+.global add
+
+add:
+    mov $6,   %rax      # 将立即数6保存到 rax 寄存器中
+    ret                 # 返回 rax 寄存器的值
+    
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
 #include "main.h"
 #include "test.h"
+#include <iostream>
+#include <stdio.h>
+
+extern "C" int add(int a, int b);
 
 int main(void) {
-    std::cout << "hello" << std::endl;
-    print_test();
-    return 0;
+  int a = 10, b = 20;
+  int result = add(a, b);
+  std::cout << "result = " << result << std::endl;
+  std::cout << "hello" << std::endl;
+  print_test();
+  return 0;
 }
